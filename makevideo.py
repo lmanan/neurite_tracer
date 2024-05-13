a8410c952cc09d1c068d3ca7bff2de974d3d3b8c
Kristina Karandasheva <kristina@Kristinas-MacBook-Pro.local>
2024-05-14 00:13:21 +0200

    fixed "stripes", change dims: 512x512 crop to 256x256 with bicubic interpolation

--- makevideo.py
+++ makevideo.py
@@ -1,11 +1,13 @@
 from pylibCZIrw import czi as pyczi
+from scipy.ndimage import zoom
 from matplotlib import pyplot as plt
 from matplotlib.patches import Patch
 from PIL import Image
 import matplotlib.cm as cm
 import numpy as np
 
-filepath = '/group/dl4miacourse/projects/neurite_trace/#####/Experiment-144.czi'
+project_path = '/Volumes/T7 Shield/20240503/'
+filepath = '/Volumes/T7 Shield/20240503/Experiment-144.czi'
 
 x_size = 500
 y_size = 500
@@ -15,7 +17,7 @@ tile_margin_x = 139
 tile_margin_y = 143
 time_channel = 145
 
-scene_of_interest = [9]
+scene_of_interest = [0]
 
 with pyczi.open_czi(filepath) as czidoc:
 
@@ -37,46 +39,18 @@ with pyczi.open_czi(filepath) as czidoc:
                     center_tile = czidoc.read(plane={"T": time_point, "Z": 3, "C": 0}, roi=box)
                     print(center_tile.shape) 
                     
-                    crop_array = center_tile.reshape(6, 256, 4, 256).swapaxes(1, 2) #
+                    #crop_array = center_tile.reshape(4, 256, 6, 256).swapaxes(1, 2)
+                    crop_array = center_tile.reshape(2, 512, 3, 512).swapaxes(1, 2)#
                     # crop_0_0 = crop_array[0, 0] #
                     crops = [crop_array[i, j] for i in range(crop_array.shape[0]) for j in range(crop_array.shape[1])] 
                     for count, crop in enumerate(crops):
-                        file_name = 'scene_' + scene_id + '_tile_' + x_tile + '_' + y_tile + '_crop_' + count + '_time_' + time_point'.tiff'
-                        im = Image.fromarray(np.squeeze(crop, axis=2))
-                        im.save("/group/dl4miacourse/projects/neurite_trace/##/" + file_name)
-                
- """                for moment in range(145):
-                    if x_tile == 0:
-                        frame_0 = czidoc.read(plane={"T": moment, "Z": 3, "C": 0}, roi=box)
-                        fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-                        ax.imshow(frame_0[..., 0], cmap=cm.Grays)
-                        plt.show()
-
-                        #im = Image.fromarray(np.squeeze(frame_0, axis=2))
-                        #im.save("data/OICE/test.tiff")
-
+                        file_name = 'scene_' + str(scene_id) + '_tile_' + str(x_tile) + '_' + str(y_tile) + '_crop_' + str(count) + '_time_' + str(time_point) + '.tiff'
+                        #im = Image.fromarray(crop)
+                        #im.save(project_path + 'pics/256_256_original/' + file_name)
+                        #for 512
+                        zoom_factor = [n / o for n, o in zip((256,256), crop.shape)]
+                        resized_crop = zoom(crop, zoom_factor, order=3)  # 'order=3' for bicubic interpolation
+                        im = Image.fromarray(resized_crop)
+                        im.save(project_path + 'pics/256_256_from_512_512/' + file_name)
 
                         k = 3
-
-        k = 3
-
-        x_pieces = scene[2] // x_size
-        y_pieces = scene[3] // y_size
-        x_remainder = scene[2] % x_size
-        y_remainder = scene[3] % y_size
-
-        for yy in range(y_pieces + (1 if y_remainder > 0 else 0)):
-            for xx in range(x_pieces + (1 if x_remainder > 0 else 0)):
-                left = scene[0] + (xx * x_size)
-                upper = scene[1] + (yy * y_size)
-                right = min(left + x_size, scene[0] + scene[2])
-                lower = min(upper + y_size, scene[1] + scene[3])
-                box = (left, upper, right-left, lower-upper)
-
-                frame_1 = czidoc.read(plane={"T": 0, "Z": 3, "C": 0}, roi=box)
-                fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-                ax.imshow(frame_1[..., 0], cmap=cm.Grays)
-                plt.show()
-                k=3
-
-k = 3 """
